7413193ab0e4e5f3531055046dfcd5204092dc70
Владислав Евгеньевич Сидоров <vlad.s200@mail.ru>
2024-10-26 12:57:29 +0500

    Пересоздание

---
+++ DataBase/DataBase/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SQLite;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataBase
+{
+    public partial class Form1 : Form
+    {
+        private SQLiteConnection sqliteConn;
+        public Form1()
+        {
+            InitializeComponent();
+            sqliteConn = new SQLiteConnection("Data Source=bd.db;Version=3;");
+            sqliteConn.Open();
+        }
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            sqliteConn.Close();
+        }
+    }
+}
